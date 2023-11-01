ae6b82b0a15e6f7171c1806850aac215f594098f
AAHAZRED <451@gmx.eu>
2023-11-01 17:17:24 +0100

    start project!

---
+++ lib/Config/INI/AccVars.pm
@@ -0,0 +1,115 @@
+package Config::INI::AccVars;
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+sub parse_ini {
+  # my ($foo, $bar, $baz) = @{{@_}}{qw(foo bar baz)};
+  my ($src) = @{{@_}}{qw(src)};
+}
+
+
+1; # End of Config::INI::AccVars
+
+
+
+__END__
+
+
+=pod
+
+
+=head1 NAME
+
+Config::INI::AccVars - The great new Config::INI::AccVars!
+
+=head1 VERSION
+
+Version 0.01
+
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Config::INI::AccVars;
+
+    my $foo = Config::INI::AccVars->new();
+    ...
+
+
+=head1 DESCRIPTION
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+
+
+=head1 AUTHOR
+
+Abdul al Hazred, C<< <451 at gmx.eu> >>
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-config-ini-accvars at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Config-INI-AccVars>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Config::INI::AccVars
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Config-INI-AccVars>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Config-INI-AccVars>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Config-INI-AccVars>
+
+=item * GitHub Repository
+
+  XXXXXXXXX
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2023 by Abdul al Hazred.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
