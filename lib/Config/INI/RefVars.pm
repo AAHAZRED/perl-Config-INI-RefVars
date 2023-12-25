3bb511463bd8fac3ffa75044ccf6bd599ab2e308
AAHAZRED <451@gmx.eu>
2023-12-25 16:03:26 +0100

    corrected release date / adapted version info

--- lib/Config/INI/RefVars.pm
+++ lib/Config/INI/RefVars.pm
@@ -9,7 +9,7 @@ use feature ":5.10";
 use Config;
 use File::Spec::Functions qw(catdir rel2abs splitpath);
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use constant DFLT_TOCOPY_SECTION  => "__TOCOPY__";
 
@@ -492,7 +492,7 @@ Config::INI::RefVars - INI file reader, allows the referencing of INI and enviro
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =head1 SYNOPSIS
 
